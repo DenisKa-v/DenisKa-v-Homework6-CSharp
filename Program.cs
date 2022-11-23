32a2c70a2d8b4fddc76a7a7f782cafceb05a154a
DenisKa <pochta_dk@mail.ru>
2022-11-23 19:44:51 +0300

    Add task43

--- Program.cs
+++ Program.cs
@@ -29,3 +29,37 @@ Console.Write ($"You entered {res} numbers greater than 0 out of {m} numbers.");
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+/* 
+void IntersectionPoint (double b1, double k1, double b2, double k2)
+{
+    if (b1 == b2 && k1 == k2)
+    {
+        Console.Write ("Straight lines match. No intersection point.");
+    }
+    else if (k1 == k2)
+    {
+        Console.Write ("Straight lines are parallel. No intersection point.");
+    }
+    else
+    {
+    double x = Math.Round(((b2 - b1) / (k1 - k2)), 1);
+    double y = Math.Round((k1 * x + b1), 1);
+    // double y2 = Math.Round((k2 * x + b2), 1);
+    Console.Write ($"Straight lines intersect. Intersection point (x; y): ({x}; {y}).");
+    }
+}
+
+Console.Write ("Input b1 in the equation y = k1 * x + b1: ");
+double b1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Input k1 in the equation y = k1 * x + b1: ");
+double k1 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Input b2 in the equation y = k2 * x + b2: ");
+double b2 = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Input k2 in the equation y = k2 * x + b2: ");
+double k2 = Convert.ToInt32 (Console.ReadLine());
+
+IntersectionPoint (b1, k1, b2, k2);
+ */
